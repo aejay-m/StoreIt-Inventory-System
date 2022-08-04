be3d1c64d69bba09cc55b1394de5b7675bc544c1
Angeline Morales <99750513+aejay-m@users.noreply.github.com>
2022-08-04 22:14:31 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+using StoreIT.v1.Layer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace StoreIT.v1
+{
+    static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+   //       Application.Run(new LoginForm());
+          Application.Run(new Dashboard());
+        }
+    }
+}
